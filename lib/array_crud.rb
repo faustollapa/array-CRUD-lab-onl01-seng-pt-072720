f3a50ac63f22874207b4de51d49d75f2c62461f0
Fausto Llapa <faustollapa850@gmail.com>
2020-08-01 17:13:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array << "Array"
-  puts 
+  puts add_element_to_end_of_array
 end
 
 def add_element_to_start_of_array(array, element)
